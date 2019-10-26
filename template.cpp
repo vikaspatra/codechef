697c537dcc4a0873cf5d656fe71d2adbfe4c56d1
vikaspatra <43319235+vikaspatra@users.noreply.github.com>
2019-10-27 01:09:54 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define MOD 1000000007
+#define boost ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define PI pair<ll,ll>
+#define ff first
+#define ss second
+
+int main()
+{
+    boost;
+    #ifndef ONLINE_JUDGE
+        freopen("custom.txt","r",stdin);
+        //freopen("output.txt","w",stdout);
+    #endif // ONLINE_JUDGE
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+
+    }
+}
---
+++ test.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define MOD 1000000007
+#define boost ios_base::sync_with_stdio(false);cin.tie(NULL)
+#define PI pair<ll,ll>
+#define ff first
+#define ss second
+
+vector<unordered_map<ll,ll> > childCount;
+unordered_map<ll,ll> tempMap;
+int main()
+{
+    boost;
+    #ifndef ONLINE_JUDGE
+        freopen("custom.txt","r",stdin);
+        //freopen("output.txt","w",stdout);
+    #endif // ONLINE_JUDGE
+
+
+
+    childCount.push_back(tempMap);
+    childCount.push_back(tempMap);
+    childCount.push_back(tempMap);
+    childCount.push_back(tempMap);
+
+
+    (childCount[2])[3]=99;
+
+    int n = (childCount[2])[3];
+
+    cout<<n<<endl;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
